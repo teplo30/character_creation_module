d6f6afde4661785b5ade1bc381b4f0354076c4fe
Василий Захаров <teplo30@yandex.ru>
2023-01-18 18:54:15 +0300

    add working version without classes

--- main.py
+++ main.py
@@ -84,12 +84,12 @@ def choice_char_class() -> str:
 def main():
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
-    char_name = input('...назови себя: ')
+    char_name: str = input('...назови себя: ')
     print(f'Здравствуй, {char_name}! '
           'Сейчас твоя выносливость — 80, атака — 5 и защита — 10.')
     print('Ты можешь выбрать один из трёх путей силы:')
     print('Воитель, Маг, Лекарь')
-    char_class = choice_char_class()
+    char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
 
 
